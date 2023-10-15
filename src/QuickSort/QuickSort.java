5d5ef1986f9f0a4aeb96737028efd67878617276
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 14:34:18 +0530

    quick sort done

--- src/QuickSort/QuickSort.java
+++ src/QuickSort/QuickSort.java
@@ -1,7 +1,46 @@
 package QuickSort;
 
 public class QuickSort {
-    public static void quickSort(int[] arr, int low, int high) {
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return; // The array is already sorted or empty
+        }
 
+        int n = arr.length;
+        quickSort(arr, 0, n - 1); // Call the helper function to perform the sort
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high); // Find the pivot index
+
+            // Recursively sort elements before and after the pivot
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; // Choose the pivot as the last element
+        int i = low - 1; // Index of the smaller element
+
+        for (int j = low; j < high; j++) {
+            // If the current element is smaller than the pivot
+            if (arr[j] < pivot) {
+                i++;
+
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swap arr[i+1] and arr[high] (or the pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1; // Return the pivot index
     }
 }
