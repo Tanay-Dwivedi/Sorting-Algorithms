5a1db8bfb76a1003f97def240fd51bb98cabaa4a
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 14:49:36 +0530

    gnome sort done

--- src/GnomeSort/GnomeSort.java
+++ src/GnomeSort/GnomeSort.java
@@ -1,7 +1,28 @@
 package GnomeSort;
 
 public class GnomeSort {
-    public static void gnomeSort(int [] arr) {
+    public static void gnomeSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return; // The array is already sorted or empty
+        }
 
+        int n = arr.length;
+        int index = 0;
+
+        while (index < n) {
+            if (index == 0) {
+                index++;
+            }
+
+            if (arr[index] >= arr[index - 1]) {
+                index++;
+            } else {
+                // Swap the out-of-place elements
+                int temp = arr[index];
+                arr[index] = arr[index - 1];
+                arr[index - 1] = temp;
+                index--;
+            }
+        }
     }
 }
