680b9637e86ae822830d8bb75f0dff5b4392948a
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 14:40:50 +0530

    bucket sort done

--- src/BucketSort/BucketSort.java
+++ src/BucketSort/BucketSort.java
@@ -1,7 +1,47 @@
 package BucketSort;
-
+import java.util.*;
 public class BucketSort {
-    public static void bucketSort(int [] arr) {
+    public static void bucketSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return; // The array is already sorted or empty
+        }
 
+        int n = arr.length;
+        int maxVal = findMaxValue(arr);
+
+        // Create an array of buckets
+        List<List<Integer>> buckets = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            buckets.add(new ArrayList<>());
+        }
+
+        // Distribute elements into buckets
+        for (int j : arr) {
+            int index = (int) ((j / (double) maxVal) * (n - 1));
+            buckets.get(index).add(j);
+        }
+
+        // Sort each bucket using insertion sort or any other sorting algorithm
+        for (List<Integer> bucket : buckets) {
+            Collections.sort(bucket);
+        }
+
+        // Concatenate sorted buckets to get the final sorted array
+        int index = 0;
+        for (List<Integer> bucket : buckets) {
+            for (int value : bucket) {
+                arr[index++] = value;
+            }
+        }
+    }
+
+    private static int findMaxValue(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
     }
 }
