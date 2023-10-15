6c731ca9eaaf566e5fc9c4854a124264142ce3a3
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 14:26:46 +0530

    selection sort done

--- src/SelectionSort/SelectionSort.java
+++ src/SelectionSort/SelectionSort.java
@@ -1,9 +1,25 @@
 package SelectionSort;
 
-import java.util.Arrays;
-
 public class SelectionSort {
-    public static void selectionSort(int [] arr) {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
 
+        for (int i = 0; i < n - 1; i++) {
+            // Assume the current index 'i' contains the minimum value
+            int minIndex = i;
+
+            // Find the index of the minimum element in the remaining unsorted portion of the array
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    // If a smaller element is found, update the index of the minimum
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the element at the current index (i)
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
     }
 }
