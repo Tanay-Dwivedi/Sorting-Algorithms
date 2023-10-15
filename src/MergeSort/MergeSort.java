ccc9d8cd647c28e90c8b47a990a58d87a55681ab
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 14:32:01 +0530

    merge sort done

--- src/MergeSort/MergeSort.java
+++ src/MergeSort/MergeSort.java
@@ -1,7 +1,54 @@
 package MergeSort;
 
 public class MergeSort {
-    public static void mergeSort(int [] arr) {
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return; // The array is already sorted or empty
+        }
 
+        int n = arr.length;
+        int[] temp = new int[n]; // Create a temporary array to store merged values
+        mergeSort(arr, temp, 0, n - 1); // Call the helper function to perform the sort
+    }
+
+    private static void mergeSort(int[] arr, int[] temp, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2; // Find the middle of the array
+
+            // Recursively sort the left and right halves
+            mergeSort(arr, temp, left, mid);
+            mergeSort(arr, temp, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, temp, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int[] temp, int left, int mid, int right) {
+        // Copy data to temporary arrays temp[] and temp[]
+        System.arraycopy(arr, left, temp, left, right - left + 1);
+
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        // Merge the two subarrays back into the original array
+        while (i <= mid && j <= right) {
+            if (temp[i] <= temp[j]) {
+                arr[k] = temp[i];
+                i++;
+            } else {
+                arr[k] = temp[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of the left subarray, if any
+        while (i <= mid) {
+            arr[k] = temp[i];
+            i++;
+            k++;
+        }
     }
 }
