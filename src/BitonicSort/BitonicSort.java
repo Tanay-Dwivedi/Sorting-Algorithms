bcfc72269d740993ddcbc45fba2b969e153be8f2
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 15:57:24 +0530

    bitonic sort done

--- src/BitonicSort/BitonicSort.java
+++ src/BitonicSort/BitonicSort.java
@@ -1,7 +1,53 @@
 package BitonicSort;
-
+import java.util.*;
 public class BitonicSort {
-    public static void bitonicSort(int[] arr) {
+    public static void bitonicSort(int[] array) {
+        if (array.length <= 1) {
+            return;
+        }
 
+        // Divide the input array into two halves.
+        int[] leftHalf = Arrays.copyOf(array, array.length / 2);
+        int[] rightHalf = Arrays.copyOfRange(array, array.length / 2, array.length);
+
+        // Recursively sort the two halves.
+        bitonicSort(leftHalf);
+        bitonicSort(rightHalf);
+
+        // Merge the two sorted halves together.
+        merge(array, leftHalf, rightHalf);
     }
+
+    private static void merge(int[] array, int[] leftHalf, int[] rightHalf) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < leftHalf.length && j < rightHalf.length) {
+            if (leftHalf[i] < rightHalf[j]) {
+                array[k] = leftHalf[i];
+                i++;
+            } else {
+                array[k] = rightHalf[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        // Add any remaining elements from the left half.
+        while (i < leftHalf.length) {
+            array[k] = leftHalf[i];
+            i++;
+            k++;
+        }
+
+        // Add any remaining elements from the right half.
+        while (j < rightHalf.length) {
+            array[k] = rightHalf[j];
+            j++;
+            k++;
+        }
+    }
+
 }
