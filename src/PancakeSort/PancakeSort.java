e37d2d2d3a2cc70343aada64e9ac87c85e921626
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 16:45:46 +0530

    pancake sort done

--- src/PancakeSort/PancakeSort.java
+++ src/PancakeSort/PancakeSort.java
@@ -1,12 +1,16 @@
 package PancakeSort;
 
 public class PancakeSort {
+    // Sorts the input array using the Pancake Sort algorithm.
     public static void pancakeSort(int[] arr) {
         int n = arr.length;
 
+        // Iterate through the array to sort it.
         for (int currSize = n; currSize > 1; currSize--) {
+            // Find the index of the maximum element in the current unsorted subarray.
             int maxIndex = findMaxIndex(arr, currSize);
 
+            // If the maximum element is not at the end of the subarray, perform flips to move it to the end.
             if (maxIndex != currSize - 1) {
                 flip(arr, maxIndex);
                 flip(arr, currSize - 1);
@@ -14,9 +18,11 @@ public class PancakeSort {
         }
     }
 
+    // Finds the index of the maximum element in the given subarray of arr.
     private static int findMaxIndex(int[] arr, int n) {
         int maxIndex = 0;
         for (int i = 0; i < n; i++) {
+            // Update maxIndex if a larger element is found.
             if (arr[i] > arr[maxIndex]) {
                 maxIndex = i;
             }
@@ -24,9 +30,11 @@ public class PancakeSort {
         return maxIndex;
     }
 
+    // Reverses the elements in the subarray arr[0...i].
     private static void flip(int[] arr, int i) {
         int start = 0;
         while (start < i) {
+            // Swap elements between start and i.
             int temp = arr[start];
             arr[start] = arr[i];
             arr[i] = temp;
