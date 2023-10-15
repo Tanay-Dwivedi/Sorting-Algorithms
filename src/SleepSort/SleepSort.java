2d8b1dc764d895362cf2feda26eba19da543b0f6
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 17:47:57 +0530

    sleep sort done

--- src/SleepSort/SleepSort.java
+++ src/SleepSort/SleepSort.java
@@ -1,7 +1,29 @@
 package SleepSort;
 
-public class SleepSort {
-    public static void sleepSort(int [] arr) {
+import java.util.concurrent.CountDownLatch;
 
+public class SleepSort {
+    public static void sleepSort(int[] arr) {
+        CountDownLatch doneSignal = new CountDownLatch(arr.length);
+
+        for (int num : arr) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(num * 100L); // Sleep for num * 100 milliseconds
+                    System.out.print(num + " ");
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    doneSignal.countDown();
+                }
+            }).start();
+        }
+
+        try {
+            doneSignal.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("\nbelow is the original array");
     }
 }
