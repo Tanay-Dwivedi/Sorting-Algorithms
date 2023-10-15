f213d38b3e20c41106562ed8860af2e62e659eab
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 14:48:34 +0530

    bogo sort done

--- src/BogoSort/BogoSort.java
+++ src/BogoSort/BogoSort.java
@@ -1,7 +1,34 @@
 package BogoSort;
 
-public class BogoSort {
-    public static void bogoSort(int [] arr) {
+import java.util.Random;
 
+public class BogoSort {
+    public static void bogoSort(int[] arr) {
+        while (!isSorted(arr)) {
+            shuffle(arr);
+        }
+    }
+
+    private static boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void shuffle(int[] arr) {
+        Random rand = new Random();
+        for (int i = arr.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+            swap(arr, i, index);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
