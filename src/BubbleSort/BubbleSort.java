7a016565b70676060f61b89f5f91bc76068ee5e4
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 14:28:47 +0530

    bubble sort done

--- src/BubbleSort/BubbleSort.java
+++ src/BubbleSort/BubbleSort.java
@@ -1,7 +1,28 @@
 package BubbleSort;
 
 public class BubbleSort {
-    public static void bubbleSort(int [] arr) {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
 
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            // Compare adjacent elements and swap if they are in the wrong order
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were swapped in the inner loop, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
     }
 }
