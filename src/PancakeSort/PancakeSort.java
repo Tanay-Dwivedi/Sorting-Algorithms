e330474426a38e2de981034d68f4ad457789ff5f
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-15 15:58:39 +0530

    pancake sort done

--- src/PancakeSort/PancakeSort.java
+++ src/PancakeSort/PancakeSort.java
@@ -1,7 +1,37 @@
 package PancakeSort;
 
 public class PancakeSort {
-    public static void pancakeSort(int [] arr) {
+    public static void pancakeSort(int[] arr) {
+        int n = arr.length;
 
+        for (int currSize = n; currSize > 1; currSize--) {
+            int maxIndex = findMaxIndex(arr, currSize);
+
+            if (maxIndex != currSize - 1) {
+                flip(arr, maxIndex);
+                flip(arr, currSize - 1);
+            }
+        }
+    }
+
+    private static int findMaxIndex(int[] arr, int n) {
+        int maxIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > arr[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
+    private static void flip(int[] arr, int i) {
+        int start = 0;
+        while (start < i) {
+            int temp = arr[start];
+            arr[start] = arr[i];
+            arr[i] = temp;
+            start++;
+            i--;
+        }
     }
 }
