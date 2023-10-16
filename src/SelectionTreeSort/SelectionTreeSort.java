1ad44602d5918740d69f7f72386850de284c785a
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-16 10:42:08 +0530

    selection tree sort done

--- src/SelectionTreeSort/SelectionTreeSort.java
+++ src/SelectionTreeSort/SelectionTreeSort.java
@@ -1,7 +1,53 @@
 package SelectionTreeSort;
 
-public class SelectionTreeSort {
-    public static void selectionTreeSort(int [] arr) {
 
+public class SelectionTreeSort {
+
+    public static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static TreeNode insert(TreeNode root, int data) {
+        if (root == null) {
+            return new TreeNode(data);
+        }
+
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    public static TreeNode buildSelectionTree(int[] array) {
+        TreeNode root = null;
+        for (int value : array) {
+            root = insert(root, value);
+        }
+        return root;
+    }
+
+    public static void inOrderTraversal(TreeNode root, int[] array, int[] index) {
+        if (root != null) {
+            inOrderTraversal(root.left, array, index);
+            array[index[0]++] = root.data;
+            inOrderTraversal(root.right, array, index);
+        }
+    }
+
+    public static void selectionTreeSort(int[] array) {
+        TreeNode root = buildSelectionTree(array);
+        int[] index = {0};
+        inOrderTraversal(root, array, index);
     }
 }
