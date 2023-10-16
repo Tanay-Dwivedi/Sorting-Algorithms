cda86457dd73ec7f3f0cc3afc0e46374b9566240
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-16 10:48:03 +0530

    smooth sort done

--- src/SmoothSort/SmoothSort.java
+++ src/SmoothSort/SmoothSort.java
@@ -1,7 +1,36 @@
 package SmoothSort;
 
 public class SmoothSort {
-    public static void smoothSort(int [] arr) {
+    public static void smoothSort(int[] array) {
+        int n = array.length;
 
+        int p = 1;
+
+        // Calculate initial p value according to the Smooth Sort formula
+        while (p < n) {
+            p = (3 * p) + 1;
+        }
+        p /= 3;
+
+        while (p > 0) {
+            for (int i = p; i < n; i++) {
+                int t = array[i];
+                int j = i;
+
+                // Perform insertion sort-like operation
+                for (; j >= p && t < array[j - p]; j -= p) {
+                    array[j] = array[j - p];
+                }
+
+                // Insert the stored t value into the last known position
+                array[j] = t;
+            }
+
+            if (p == 1) {
+                p = 0;
+            } else {
+                p /= 3;
+            }
+        }
     }
 }
