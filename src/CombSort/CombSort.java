c1789ebb15ecea172ab8b46d239a6d9b42ec63a9
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-16 10:35:43 +0530

    comb sort done

--- src/CombSort/CombSort.java
+++ src/CombSort/CombSort.java
@@ -1,7 +1,35 @@
 package CombSort;
 
 public class CombSort {
-    public static void combSort(int [] arr) {
+    public static void combSort(int[] array) {
+        int gap = array.length;
+        boolean swapped = true;
+
+        while (gap > 1 || swapped) {
+            // Calculate the gap using the shrink factor (typically 1.3)
+            gap = getNextGap(gap);
+
+            swapped = false;
+
+            // Compare elements with the calculated gap
+            for (int i = 0; i < array.length - gap; i++) {
+                if (array[i] > array[i + gap]) {
+                    // Swap elements if they are out of order
+                    int temp = array[i];
+                    array[i] = array[i + gap];
+                    array[i + gap] = temp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+
+    private static int getNextGap(int gap) {
+        // Shrink the gap using a common shrink factor, typically 1.3
+        gap = (gap * 10) / 13;
+
+        // Ensure the gap is at least 1
+        return Math.max(gap, 1);
 
     }
 }
