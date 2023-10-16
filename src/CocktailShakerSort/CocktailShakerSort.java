39ea70a1c29cf123322117b764ef0723de17d814
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-16 10:39:28 +0530

    cocktail shaker sort done

--- src/CocktailShakerSort/CocktailShakerSort.java
+++ src/CocktailShakerSort/CocktailShakerSort.java
@@ -1,7 +1,37 @@
 package CocktailShakerSort;
 
 public class CocktailShakerSort {
-    public static void cocktailShakerSort(int [] arr) {
+    public static void cocktailShakerSort(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        boolean swapped = true;
 
+        while (swapped) {
+            swapped = false;
+
+            // Move from left to right
+            for (int i = left; i < right; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                    swapped = true;
+                }
+            }
+            right--;
+
+            // Move from right to left
+            for (int i = right; i > left; i--) {
+                if (array[i] < array[i - 1]) {
+                    swap(array, i, i - 1);
+                    swapped = true;
+                }
+            }
+            left++;
+        }
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
