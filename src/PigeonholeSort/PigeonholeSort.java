c17d5b7c4ceaa664f4c2f0659de1468c6855c7f9
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-16 10:38:12 +0530

    Pigeonhole sort done

--- src/PigeonholeSort/PigeonholeSort.java
+++ src/PigeonholeSort/PigeonholeSort.java
@@ -1,7 +1,28 @@
 package PigeonholeSort;
 
-public class PigeonholeSort {
-    public static void pigeonholeSort(int [] arr) {
+import java.util.Arrays;
 
+public class PigeonholeSort {
+    public static void pigeonholeSort(int[] array) {
+        int min = Arrays.stream(array).min().getAsInt();
+        int max = Arrays.stream(array).max().getAsInt();
+        int range = max - min + 1;
+
+        int[] pigeonholes = new int[range];
+
+        // Place each element into its corresponding pigeonhole
+        for (int value : array) {
+            pigeonholes[value - min]++;
+        }
+
+        // Reconstruct the sorted array from the pigeonholes
+        int index = 0;
+        for (int i = 0; i < range; i++) {
+            while (pigeonholes[i] > 0) {
+                array[index] = i + min;
+                pigeonholes[i]--;
+                index++;
+            }
+        }
     }
 }
